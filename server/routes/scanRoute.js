3b1c31f52fa4be2b8829e404619f39da4e6f34d9
Aditya Bargujar <144697498+AdityaBargujar@users.noreply.github.com>
2025-08-11 23:27:16 +0530

    Delete server directory

--- server/routes/scanRoute.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { scanPage } = require('../controllers/scanController');
-
-router.post('/scan', scanPage);
-
-module.exports = router;
